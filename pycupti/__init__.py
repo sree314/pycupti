7615b1a9fa91272485a32385783279f459088eb7
Sreepathi Pai <sreepai@ices.utexas.edu>
2017-04-23 17:04:36 -0500

    Add support for introspection of events and event domains

--- pycupti/__init__.py
+++ pycupti/__init__.py
@@ -126,3 +126,258 @@ def cuptiGetResultString(result):
         return ctypes.c_char_p(cp.value).value
     else:
         raise CUPTIError(r)
+
+# CUptiResult cuptiDeviceGetNumEventDomains ( CUdevice device, uint32_t* numDomains ) 
+_cuptiDeviceGetNumEventDomains = cupti['cuptiDeviceGetNumEventDomains']
+_cuptiDeviceGetNumEventDomains.restype = CUptiResult
+_cuptiDeviceGetNumEventDomains.argtypes = [CUdevice,
+                                           ctypes.POINTER(ctypes.c_uint32)]
+
+def cuptiDeviceGetNumEventDomains(device= 0):
+    d = CUdevice(device)
+    n = ctypes.c_uint32(0)
+    r = _cuptiDeviceGetNumEventDomains(d, n)
+
+    if r == CUPTI_SUCCESS:
+        return n.value
+    else:
+        raise CUPTIError(r)
+
+# CUptiResult cuptiEventDomainGetNumEvents ( CUpti_EventDomainID eventDomain, uint32_t* numEvents ) 
+_cuptiEventDomainGetNumEvents = cupti['cuptiEventDomainGetNumEvents']
+_cuptiEventDomainGetNumEvents.restype = CUptiResult
+_cuptiEventDomainGetNumEvents.argtypes = [CUpti_EventDomainID,
+                                           ctypes.POINTER(ctypes.c_uint32)]
+
+def cuptiEventDomainGetNumEvents(eventDomain):
+    d = CUpti_EventDomainID(eventDomain)
+    n = ctypes.c_uint32(0)
+    r = _cuptiEventDomainGetNumEvents(d, n)
+
+    if r == CUPTI_SUCCESS:
+        return n.value
+    else:
+        raise CUPTIError(r)
+
+# CUptiResult cuptiDeviceEnumEventDomains ( CUdevice device, size_t* arraySizeBytes, CUpti_EventDomainID* domainArray ) 
+
+_cuptiDeviceEnumEventDomains = cupti["cuptiDeviceEnumEventDomains"]
+_cuptiDeviceEnumEventDomains.restype = CUptiResult
+_cuptiDeviceEnumEventDomains.argtypes = [CUdevice, ctypes.POINTER(ctypes.c_size_t), ctypes.POINTER(CUpti_EventDomainID)]
+
+def cuptiDeviceEnumDomains(device, ndomains):
+    a = ctypes.c_size_t(ndomains * ctypes.sizeof(CUpti_EventDomainID))
+    did = (CUpti_EventDomainID * ndomains)()
+
+    r = _cuptiDeviceEnumEventDomains(device, a, did)
+
+    if r == CUPTI_SUCCESS:
+        assert a.value == ctypes.sizeof(did), "written bytes %d != %d" % (a.value, ctypes.sizeof(did))
+
+        return did
+    else:
+        raise CUPTIError(r)
+
+# CUptiResult cuptiEnumEventDomains ( size_t* arraySizeBytes, CUpti_EventDomainID* domainArray ) 
+_cuptiEnumEventDomains = cupti["cuptiEnumEventDomains"]
+_cuptiEnumEventDomains.restype = CUptiResult
+_cuptiEnumEventDomains.argtypes = [ctypes.POINTER(ctypes.c_size_t), ctypes.POINTER(CUpti_EventDomainID)]
+
+def cuptiEnumEventDomains(ndomains):
+    a = ctypes.c_size_t(ndomains * ctypes.sizeof(CUpti_EventDomainID))
+    did = (CUpti_EventDomainID * ndomains)()
+    
+    r = _cuptiEnumEventDomains(a, did)
+
+    if r == CUPTI_SUCCESS:
+        assert a.value == ctypes.sizeof(did), "written bytes %d != %d" % (a.value, ctypes.sizeof(did))
+        return did
+    else:
+        raise CUPTIError(r)
+
+# CUptiResult cuptiEventDomainEnumEvents ( CUpti_EventDomainID eventDomain, size_t* arraySizeBytes, CUpti_EventID* eventArray ) 
+_cuptiEventDomainEnumEvents = cupti["cuptiEventDomainEnumEvents"]
+_cuptiEventDomainEnumEvents.restype = CUptiResult
+_cuptiEventDomainEnumEvents.argtypes = [CUpti_EventDomainID, ctypes.POINTER(ctypes.c_size_t), ctypes.POINTER(CUpti_EventID)]
+
+def cuptiEventDomainEnumEvents(eventDomain, nevents):
+    a = ctypes.c_size_t(nevents * ctypes.sizeof(CUpti_EventID))
+    eid = (CUpti_EventID * nevents)()
+    
+    r = _cuptiEventDomainEnumEvents(eventDomain, a, eid)
+
+    if r == CUPTI_SUCCESS:
+        assert a.value == ctypes.sizeof(eid), "written bytes %d != %d" % (a.value, ctypes.sizeof(eid))
+        return eid
+    else:
+        raise CUPTIError(r)
+
+
+# CUptiResult cuptiEventDomainGetAttribute ( CUpti_EventDomainID eventDomain, CUpti_EventDomainAttribute attrib, size_t* valueSize, void* value ) 
+_cuptiEventDomainGetAttribute = cupti["cuptiEventDomainGetAttribute"]
+_cuptiEventDomainGetAttribute.restype = CUptiResult
+_cuptiEventDomainGetAttribute.argtypes = [CUpti_EventDomainID, CUpti_EventDomainAttribute, ctypes.POINTER(ctypes.c_size_t), ctypes.c_void_p]
+
+def cuptiEventDomainGetAttribute(eventDomain, attrib):
+    uint32_attr = set([CUPTI_EVENT_DOMAIN_ATTR_INSTANCE_COUNT, CUPTI_EVENT_DOMAIN_ATTR_TOTAL_INSTANCE_COUNT])
+    const_cstr_attr = set([CUPTI_EVENT_DOMAIN_ATTR_NAME])
+    int_attr = set([CUPTI_EVENT_DOMAIN_ATTR_COLLECTION_METHOD])
+
+    if attrib in uint32_attr:
+        value = ctypes.c_uint32()
+    elif attrib in int_attr:
+        value = ctypes.c_int()
+    elif attrib in const_cstr_attr:
+        value = ctypes.create_string_buffer(512)
+    else:
+        assert False, "Unknown attribute type"
+
+    sz = ctypes.c_size_t(ctypes.sizeof(value))
+    r = _cuptiEventDomainGetAttribute(eventDomain, attrib, sz, value)
+
+    if r != CUPTI_SUCCESS:
+        raise CUPTIError(r)
+    else:
+        if attrib in uint32_attr:
+            return value.value
+        elif attrib in int_attr:
+            return value.value
+        elif attrib in const_cstr_attr:
+            if sz.value == len(value):
+                # no null byte
+                return value[:sz.value]
+            else:
+                # discard null byte
+                return value[:sz.value-1]
+
+
+# CUptiResult cuptiEventGetIdFromName ( CUdevice device, const char* eventName, CUpti_EventID* event ) 
+_cuptiEventGetIdFromName = cupti["cuptiEventGetIdFromName"]
+_cuptiEventGetIdFromName.restype = CUptiResult
+_cuptiEventGetIdFromName.argtypes = [CUdevice, ctypes.c_char_p, ctypes.POINTER(CUpti_EventID)]
+
+def cuptiEventGetIdFromName(device, eventName):
+    evId = CUpti_EventID()
+
+    r = _cuptiEventGetIdFromName(device, eventName, evId)
+
+    if r == CUPTI_ERROR_INVALID_EVENT_NAME:
+        return None
+    elif r == CUPTI_SUCCESS:
+        return evId.value
+    else:
+        raise CUPTIError(r)
+
+def cuptiHelperGetAllEvents(d):
+    domains = cuptiDeviceGetNumEventDomains(d)
+    dids = cuptiDeviceEnumDomains(d, domains)
+
+    out = {}
+    for did in dids:
+        nevents = cuptiEventDomainGetNumEvents(did)
+        eids = cuptiEventDomainEnumEvents(did, nevents)
+        out[did] = [x for x in eids]
+
+    return out
+
+# CUptiResult cuptiEventGetAttribute ( CUpti_EventID event, CUpti_EventAttribute attrib, size_t* valueSize, void* value ) 
+_cuptiEventGetAttribute = cupti["cuptiEventGetAttribute"]
+_cuptiEventGetAttribute.restype = CUptiResult
+_cuptiEventGetAttribute.argtypes = [CUpti_EventID, CUpti_EventAttribute, ctypes.POINTER(ctypes.c_size_t), ctypes.c_void_p]
+
+def cuptiEventGetAttribute(event, attrib, strsize = 512):
+    const_cstr_attr = set([CUPTI_EVENT_ATTR_NAME, CUPTI_EVENT_ATTR_LONG_DESCRIPTION, CUPTI_EVENT_ATTR_SHORT_DESCRIPTION])
+    int_attr = set([CUPTI_EVENT_ATTR_CATEGORY])
+
+    if attrib in int_attr:
+        value = ctypes.c_int()
+        arg_value = ctypes.byref(value)
+    elif attrib in const_cstr_attr:
+        value = ctypes.create_string_buffer(strsize)
+        arg_value = value
+    else:
+        assert False, "Unknown attribute type"
+
+    sz = ctypes.c_size_t(ctypes.sizeof(value))
+    r = _cuptiEventGetAttribute(event, attrib, sz, arg_value)
+
+    if r != CUPTI_SUCCESS:
+        raise CUPTIError(r)
+    else:
+        if attrib in int_attr:
+            return value.value
+        elif attrib in const_cstr_attr:
+            if sz.value == len(value):
+                # no null byte
+                return value[:sz.value]
+            else:
+                assert sz.value > 0
+                if ord(value[sz.value - 1]) == 0:
+                    # discard null byte
+                    return value[:sz.value-1]
+                else:
+                    # buggy, CUPTI not counting null byte
+                    return value[:sz.value]
+
+# CUptiResult cuptiDeviceGetAttribute ( CUdevice device, CUpti_DeviceAttribute attrib, size_t* valueSize, void* value ) 
+_cuptiDeviceGetAttribute = cupti['cuptiDeviceGetAttribute']
+_cuptiDeviceGetAttribute.restype = CUptiResult
+_cuptiDeviceGetAttribute.argtypes = [CUdevice, CUpti_DeviceAttribute, 
+                                     ctypes.POINTER(ctypes.c_size_t),
+                                     ctypes.c_void_p]
+
+def cuptiDeviceGetAttribute(device, attrib):
+    uint32_attr = set([CUPTI_DEVICE_ATTR_MAX_EVENT_ID,
+                       CUPTI_DEVICE_ATTR_MAX_EVENT_DOMAIN_ID,
+                       CUPTI_DEVICE_ATTR_INSTRUCTION_PER_CYCLE,
+                       CUPTI_DEVICE_ATTR_NVLINK_PRESENT,
+                       ])
+                       
+    uint64_attr = set([CUPTI_DEVICE_ATTR_GLOBAL_MEMORY_BANDWIDTH,
+                       CUPTI_DEVICE_ATTR_INSTRUCTION_THROUGHPUT_SINGLE_PRECISION,
+                       CUPTI_DEVICE_ATTR_MAX_FRAME_BUFFERS,
+                       CUPTI_DEVICE_ATTR_PCIE_LINK_RATE,
+                       CUPTI_DEVICE_ATTR_PCIE_LINK_WIDTH,
+                       CUPTI_DEVICE_ATTR_PCIE_GEN,
+                       CUPTI_DEVICE_ATTR_FLOP_SP_PER_CYCLE,
+                       CUPTI_DEVICE_ATTR_FLOP_DP_PER_CYCLE,
+                       CUPTI_DEVICE_ATTR_MAX_L2_UNITS,
+                       CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_SHARED,
+                       CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_L1,
+                       CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_EQUAL,
+                       CUPTI_DEVICE_ATTR_FLOP_HP_PER_CYCLE,
+                       CUPTI_DEVICE_ATTR_GPU_CPU_NVLINK_BW,
+                   ])
+    
+    if attrib in uint32_attr:
+        arg1 = ctypes.c_uint32(0)
+        sz = ctypes.c_size_t(ctypes.sizeof(ctypes.c_uint32))
+
+        r = _cuptiDeviceGetAttribute(device, attrib, sz, ctypes.byref(arg1))
+        
+        if r != CUPTI_SUCCESS:
+            raise CUPTIError(r)
+        else:
+            if sz.value == ctypes.sizeof(ctypes.c_uint32):
+                return arg1.value
+            else:                
+                # not of type c_uint32!
+                assert False, sz.value
+    elif attrib in uint64_attr:
+        arg1 = ctypes.c_uint64(0)
+        sz = ctypes.c_size_t(ctypes.sizeof(ctypes.c_uint64))
+
+        r = _cuptiDeviceGetAttribute(device, attrib, sz, ctypes.byref(arg1))
+        
+        if r != CUPTI_SUCCESS:
+            raise CUPTIError(r)
+        else:
+            if sz.value == ctypes.sizeof(ctypes.c_uint64):
+                return arg1.value
+            else:                
+                # not of type c_uint32!
+                assert False, sz.value       
+    else:
+        raise NotImplementedError
+    
+    
--- pycupti/constants.py
+++ pycupti/constants.py
@@ -1,4 +1,5 @@
 CUPTI_SUCCESS = 0
+CUPTI_ERROR_INVALID_EVENT_NAME = 6
 
 CUPTI_METRIC_ATTR_NAME = 0 #   Metric name. Value is a null terminated const c-string. 
 CUPTI_METRIC_ATTR_SHORT_DESCRIPTION = 1 #    Short description of metric. Value is a null terminated const c-string. 
@@ -23,3 +24,86 @@ CUPTI_METRIC_VALUE_KIND_UTILIZATION_LEVEL = 5 #    The metric value is a utiliza
 
 CUPTI_METRIC_EVALUATION_MODE_PER_INSTANCE = 1 #If this bit is set, the metric can be profiled for each instance of the domain. The event values passed to cuptiMetricGetValue can contain values for one instance of the domain. And cuptiMetricGetValue can be called for each instance. 
 CUPTI_METRIC_EVALUATION_MODE_AGGREGATE = 1<<1 
+
+CUPTI_DEVICE_ATTR_MAX_EVENT_ID = 1
+#    Number of event IDs for a device. Value is a uint32_t. 
+CUPTI_DEVICE_ATTR_MAX_EVENT_DOMAIN_ID = 2
+#    Number of event domain IDs for a device. Value is a uint32_t. 
+CUPTI_DEVICE_ATTR_GLOBAL_MEMORY_BANDWIDTH = 3
+#    Get global memory bandwidth in Kbytes/sec. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_INSTRUCTION_PER_CYCLE = 4
+#    Get theoretical maximum number of instructions per cycle. Value is a uint32_t. 
+CUPTI_DEVICE_ATTR_INSTRUCTION_THROUGHPUT_SINGLE_PRECISION = 5
+#    Get theoretical maximum number of single precision instructions that can be executed per second. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_MAX_FRAME_BUFFERS = 6
+#    Get number of frame buffers for device. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_PCIE_LINK_RATE = 7
+#    Get PCIE link rate in Mega bits/sec for device. Return 0 if bus-type is non-PCIE. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_PCIE_LINK_WIDTH = 8
+#    Get PCIE link width for device. Return 0 if bus-type is non-PCIE. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_PCIE_GEN = 9
+#    Get PCIE generation for device. Return 0 if bus-type is non-PCIE. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_DEVICE_CLASS = 10
+#    Get the class for the device. Value is a CUpti_DeviceAttributeDeviceClass. 
+CUPTI_DEVICE_ATTR_FLOP_SP_PER_CYCLE = 11
+#    Get the peak single precision flop per cycle. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_FLOP_DP_PER_CYCLE = 12
+#    Get the peak double precision flop per cycle. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_MAX_L2_UNITS = 13
+#    Get number of L2 units. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_SHARED = 14
+#    Get the maximum shared memory for the CU_FUNC_CACHE_PREFER_SHARED preference. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_L1 = 15
+#    Get the maximum shared memory for the CU_FUNC_CACHE_PREFER_L1 preference. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_MAX_SHARED_MEMORY_CACHE_CONFIG_PREFER_EQUAL = 16
+#    Get the maximum shared memory for the CU_FUNC_CACHE_PREFER_EQUAL preference. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_FLOP_HP_PER_CYCLE = 17
+#    Get the peak half precision flop per cycle. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_NVLINK_PRESENT = 18
+#    Check if Nvlink is connected to device. Returns 1, if at least one Nvlink is connected to the device, returns 0 otherwise. Value is a uint32_t. 
+CUPTI_DEVICE_ATTR_GPU_CPU_NVLINK_BW = 19
+#    Check if Nvlink is present between GPU and CPU. Returns Bandwidth, in Bytes/sec, if Nvlink is present, returns 0 otherwise. Value is a uint64_t. 
+CUPTI_DEVICE_ATTR_FORCE_INT = 0x7fffffff
+
+
+CUPTI_EVENT_ATTR_NAME = 0
+#    Event name. Value is a null terminated const c-string. 
+CUPTI_EVENT_ATTR_SHORT_DESCRIPTION = 1
+#    Short description of event. Value is a null terminated const c-string. 
+CUPTI_EVENT_ATTR_LONG_DESCRIPTION = 2
+#    Long description of event. Value is a null terminated const c-string. 
+CUPTI_EVENT_ATTR_CATEGORY = 3
+#    Category of event. Value is CUpti_EventCategory. 
+CUPTI_EVENT_ATTR_FORCE_INT = 0x7fffffff
+
+CUPTI_EVENT_CATEGORY_INSTRUCTION = 0
+#    An instruction related event. 
+CUPTI_EVENT_CATEGORY_MEMORY = 1
+#    A memory related event. 
+CUPTI_EVENT_CATEGORY_CACHE = 2
+#    A cache related event. 
+CUPTI_EVENT_CATEGORY_PROFILE_TRIGGER = 3
+#    A profile-trigger event. 
+CUPTI_EVENT_CATEGORY_FORCE_INT = 0x7fffffff
+
+CUPTI_EVENT_COLLECTION_METHOD_PM = 0
+#    Event is collected using a hardware global performance monitor. 
+CUPTI_EVENT_COLLECTION_METHOD_SM = 1
+#    Event is collected using a hardware SM performance monitor. 
+CUPTI_EVENT_COLLECTION_METHOD_INSTRUMENTED = 2
+#    Event is collected using software instrumentation. 
+CUPTI_EVENT_COLLECTION_METHOD_NVLINK_TC = 3
+#    Event is collected using NvLink throughput counter method. 
+CUPTI_EVENT_COLLECTION_METHOD_FORCE_INT = 0x7fffffff
+
+CUPTI_EVENT_DOMAIN_ATTR_NAME = 0
+#    Event domain name. Value is a null terminated const c-string. 
+CUPTI_EVENT_DOMAIN_ATTR_INSTANCE_COUNT = 1
+#    Number of instances of the domain for which event counts will be collected. The domain may have additional instances that cannot be profiled (see CUPTI_EVENT_DOMAIN_ATTR_TOTAL_INSTANCE_COUNT). Can be read only with cuptiDeviceGetEventDomainAttribute. Value is a uint32_t. 
+CUPTI_EVENT_DOMAIN_ATTR_TOTAL_INSTANCE_COUNT = 3
+#    Total number of instances of the domain, including instances that cannot be profiled. Use CUPTI_EVENT_DOMAIN_ATTR_INSTANCE_COUNT to get the number of instances that can be profiled. Can be read only with cuptiDeviceGetEventDomainAttribute. Value is a uint32_t. 
+CUPTI_EVENT_DOMAIN_ATTR_COLLECTION_METHOD = 4
+#    Collection method used for events contained in the event domain. Value is a CUpti_EventCollectionMethod. 
+CUPTI_EVENT_DOMAIN_ATTR_FORCE_INT = 0x7fffffff
+
+
